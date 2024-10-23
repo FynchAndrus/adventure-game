ecafad1a040d6863149b69634fb93d0c4b564a71
FynchAndrus <fynch.andrus@umconnect.umt.edu>
2024-10-23 12:16:25 -0600

    Update gamefunctions.py
    Added doc strings to purchase_item and create_new_monster.

--- gamefunctions.py
+++ gamefunctions.py
@@ -30,6 +30,7 @@ def print_welcome(name):
     Example:
      >>>print_welcome(Joe)
           Hello Joe!     '''
+    
     width = 20
     welcome = 'Hello '+name+'!'
     return print(f'{welcome:^20}')
@@ -65,13 +66,29 @@ def print_shop_menu(item1Name,item1Price,item2Name,item2Price):
     item2 = print(f'|{item2Name:12}{item2Format:>8}|')
     footer = print('\\'+'-'*20+'/')
     
-
-'''This function will return the number of items purchased and the
-quantity of money that is remaining. If unable to afford all the
-items, it will only buy as many as can be afforded. Nothing is printed
-by the function call. The quantityToPurchase parameter has a default value of 1.'''
-
 def purchase_item(itemPrice, startingMoney, quantityToPurchase=1):
+    '''This function will return the number of items purchased and the
+    quantity of money that is remaining. If unable to afford all the items,
+    it will only buy as many as can be afforded. Nothing is printed by the
+    function call. The quantityToPurchase parameter has a default value of 1.
+    
+    Parameters:
+     itemPrice(float): Price of the item being purchased
+     startingMoney(float): How much money user has prior to the transaction
+     quantityToPurchase(int): The number of items being purchased. Has a default
+     value of 1.
+     
+    Returns:
+     quantityPurchased(int): The actual number of items purchased
+     leftoverMoney(float): How much money user has after the transaction.
+     
+    Example:
+    >>> amountPurchased, remainingMoney = purchase_item(1.25, 4, 2)
+    >>>print(amountPurchased)
+    2
+    >>>print(remainingMoney)
+    1.50'''
+    
     quantityPurchased = startingMoney // itemPrice
     if quantityPurchased > quantityToPurchase:
         quantityPurchased = quantityToPurchase
@@ -80,31 +97,53 @@ def purchase_item(itemPrice, startingMoney, quantityToPurchase=1):
     leftoverMoney = startingMoney - (itemPrice * quantityPurchased)
     return quantityPurchased, leftoverMoney
 
-'''This function will return 1 of 3 available monsters as well as its description, health,
-power, and money. The amount of money will be random but confined to a range around 15.'''
 import random
 def new_random_monster():
+    '''This function will return 1 of 3 available monsters - Sprite, Imp, Skeleton -  as well
+    as its description, health, power, and money. The amount of money, health, and power will 
+    be random but confined to a range around 15.
+    
+    Parameters:
+     None
+     
+    Returns:
+     name(str): Name of the monster created
+     description(str): Description of the monster
+     health(randint): How much health the monster begins with. Has range of 5.
+     power(randint): How much damage each monster deals. Has a range of 5.
+     money(randint): How much money each monster has. Has range of 15.
+     
+    Example:
+     >>> name, decription = new_random_monster()
+     >>>print(name)
+     Sprite
+     >>>print(description)
+     Tiny fey creature, often asscociated with the natural elements.
+     >>> name, power = new_random_monster()
+     >>> print(name, power)
+     Skeleton 14'''
+    
     monster = random.randint(1,3)
     if monster == 1:
         name = 'Sprite'
         description = 'Tiny fey creature, often associated with the natural elements.'
-        health = random.randint(12,17)
+        health = random.randint(12,16)
         power = random.randint(8,12)
-        money = random.randint(27,45)
+        money = random.randint(27,42)
         return name, description, health, power, money
     elif monster == 2:
         name = 'Imp'
         description = 'Tiny fiend (devil, shapechanger)'
         health = random.randint(11,15)
         power = random.randint(8,12)
-        money = random.randint(42,57)
+        money = random.randint(42,56)
         return name, description, health, power, money
     elif monster == 3:
        name = 'Skeleton'
        description = 'Remanants of the undead, resurrected.'
        health = random.randint(6,10)
-       power = random.randint(12,17)
-       money = random.randint(20,36)
+       power = random.randint(12,16)
+       money = random.randint(20,34)
        return name, description, health, power, money
 
 
